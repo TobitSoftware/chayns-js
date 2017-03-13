ad1a18313ae36ba980d527732c1be727fc6210da
SHoltkamp <steffen.holtkamp@tobit.software>
2017-03-13 12:58:53 +0100

    Add ChaynsCall 46 getBeaconMonitoringStatus

---
+++ src/chayns/calls/getBeaconMonitoringStatus.js
@@ -0,0 +1,27 @@
+/**
+ * Created by SHoltkamp on 08.03.2017.
+ */
+import { chaynsCall } from '../chaynsCall';
+import { getCallbackName } from '../callback';
+import { propTypes } from '../propTypes';
+
+export function getBeaconMonitoringStatus() {
+    const callbackName = 'getBeaconMonitoringStatus';
+
+    return chaynsCall({
+        'call': {
+            'action': 46,
+            'value': {
+                'callback': getCallbackName(callbackName)
+            }
+        },
+        'app': {
+            'support': {'android': 4727, 'ios': 4301}
+        },
+        'web': false,
+        callbackName,
+        'propTypes': {
+            'callback': propTypes.string.isRequired
+        }
+    });
+}
--- src/chayns/calls/index.js
+++ src/chayns/calls/index.js
@@ -19,6 +19,7 @@ export * from './scanQRCode'; // 34
 export * from './getBeaconHistory'; // 36
 export * from './setNfcCallback'; // 37/38
 export * from './getLocationBeacons'; // 39
+export * from './getBeaconMonitoringStatus'; // 46
 export * from './addToWallet'; // 47
 export * from './login'; // 54/56
 export * from './refreshAccessToken'; // 55
