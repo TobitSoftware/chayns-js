75464f5a2433e41479b91dff362b4a04d30d44f0
SHoltkamp <steffen.holtkamp@tobit.software>
2017-05-03 13:46:19 +0200

    Call 129: closeWindow This Call is only supported for ChaynsWebLight(CWL). This call wil close the CWL window of your DavidClient.

---
+++ src/chayns/closeWindow.js
@@ -0,0 +1,25 @@
+import { chaynsCall } from '../chaynsCall';
+import { getCallbackName } from '../callback';
+import { propTypes } from '../propTypes';
+
+export function closeWindow(callback){
+
+    let callbackName = 'getSavedIntercomChats';
+
+    return chaynsCall({
+        'call': {
+            'action': 129,
+            'value': {
+                'callback': getCallbackName(callbackName),
+            }
+        },
+        'app': false,
+        'web': false,
+        'cwl': {'version': 2},
+        callbackName,
+        'callbackFunction': callback,
+        'propTypes': {
+            'callback': propTypes.string.isRequired
+        }
+    });
+}
