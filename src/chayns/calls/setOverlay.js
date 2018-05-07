c7fc98e10d6537d13a8bc1dcc995c18815584944
SHoltkamp <steffen.holtkamp@tobit.software>
2018-05-07 08:42:41 +0200

    Call 116: setOverlay Add a new Parameter mode to exclude tapp from overlay.

--- src/chayns/calls/setOverlay.js
+++ src/chayns/calls/setOverlay.js
@@ -2,32 +2,33 @@ import { chaynsCall } from '../chaynsCall';
 import { getCallbackName } from '../callback';
 import { propTypes } from '../propTypes';
 
-function setOverlay(enabled, color, transition) {
-	const callbackName = 'showOverlay';
+function setOverlay(enabled, color, transition, mode) {
+    const callbackName = 'showOverlay';
 
-	return chaynsCall({
-		call: {
-			'action': 116,
-			'value': {
-				'callback': getCallbackName(callbackName),
-				enabled,
-				color,
-				transition,
-			}
-		},
-		app: false,
-		callbackName,
-		propTypes: {
-			'enabled': propTypes.boolean.isRequired,
-			'callback': propTypes.string.isRequired,
-			'color': propTypes.string,
-			'transition': propTypes.string
-		}
-	});
+    return chaynsCall({
+        'call': {
+            'action': 116,
+            'value': {
+                'callback': getCallbackName(callbackName),
+                enabled,
+                color,
+                transition,
+                mode
+            }
+        },
+        'app': false,
+        callbackName,
+        'propTypes': {
+            'enabled': propTypes.boolean.isRequired,
+            'callback': propTypes.string.isRequired,
+            'color': propTypes.string,
+            'transition': propTypes.string
+        }
+    });
 }
 
-export function showOverlay(color, transition) {
-	return setOverlay(true, color, transition);
+export function showOverlay(color, transition, mode) {
+    return setOverlay(true, color, transition, mode);
 }
 
 export function hideOverlay(transition) {
