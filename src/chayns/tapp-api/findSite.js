3c5912ab5bf1888a07188acb4a724fa9d99dc383
Jonas Gossens <jg@tobit.software>
2019-02-14 09:40:43 +0100

    :bug: Fix findSite

--- src/chayns/tapp-api/findSite.js
+++ src/chayns/tapp-api/findSite.js
@@ -1,11 +1,6 @@
 import {tappApi} from './tappApi';
-import {environment} from '../environment';
 
 export function findSite(query, skip = 0, take = 20) {
-    let u = '';
-    if(environment.user && environment.user.userId) {
-        u = '&userId=' + environment.user.userId;
-    }
-    return tappApi(`Global/Search?SearchString=${query}&LocationId=${environment.site.locationId}${u}&Skip=${skip}&Take=${take}`, true)
+    return tappApi(`Site/SlitteAppBySearchString?SearchString=${query}&Skip=${skip}&Take=${take}`, true)
         .then((json) => json);
 }
