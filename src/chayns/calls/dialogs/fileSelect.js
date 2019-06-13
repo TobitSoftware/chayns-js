8dc0d44e06a7ff617cb2cf6ed8a4da77d1a6eb5a
Heiner Pöpping <heiner.poepping@t-online.de>
2019-06-13 14:26:01 +0200

    Add file select dialog

---
+++ src/chayns/calls/dialogs/fileSelect.js
@@ -0,0 +1,27 @@
+import {buttonText, buttonType, dialogAction} from './chaynsDialog';
+import {isArray} from '../../../utils/is';
+import {isDialogPermitted} from '../../../utils/isPermitted';
+import {open} from './open';
+
+export function fileSelect(dialog) {
+    const callbackName = 'fileSelectCallback';
+
+    if (!dialog.buttons || !isArray(dialog.buttons)) {
+        dialog.buttons = [];
+        if (dialog.multiselect) {
+            dialog.buttons.push({
+                'text': buttonText.OK,
+                'buttonType': buttonType.POSITIVE
+            });
+        }
+        dialog.buttons.push({
+            'text': buttonText.CANCEL,
+            'buttonType': buttonType.NEGATIVE
+        });
+    }
+
+    if(isDialogPermitted()) {
+        dialog.callType = dialogAction.FILE_SELECT;
+        return open(dialog);
+    }
+}
