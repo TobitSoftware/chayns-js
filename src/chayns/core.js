04d4ba267f8180240b1a36a5d0f0ab1ed6a93b5d
Jonas Gossens <jonas.gossens@tobit.software>
2020-04-02 12:24:17 +0200

    :sparkles: Change setWidth call action

--- src/chayns/core.js
+++ src/chayns/core.js
@@ -234,8 +234,6 @@ function dynamicFontSize() {
     if (environment.isInFrame) {
         addWidthChangeListener(setWidthVariable);
     } else {
-
-
         window.addEventListener('message', (event) => {
             const data = event.data;
 
@@ -246,7 +244,7 @@ function dynamicFontSize() {
             let callPrefix = data.split(':', 1);
             let prefixLength = callPrefix[0].length + 1; // also cut the first :
             let params = JSON.parse(data.substr(prefixLength, data.length - prefixLength));
-            if (params.action === 234) {
+            if (params.action === 237) {
                 const frameName = callPrefix[0].split('@')[1];
                 if (!callbacks[frameName]) {
                     callbacks[frameName] = [];
